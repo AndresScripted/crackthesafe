5c3cba4b0668f7081db8393ec744d2b423779c68
Andres <andreshuz0709@gmail.com>
2018-10-19 01:23:55 +0000

    user choice works

--- js/script.js
+++ js/script.js
@@ -8,4 +8,12 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
-
+$("button").click(function() {
+    
+    var guess1 = $("#number1").val();
+    var guess2 = $("#number2").val();
+    var guess3 = $("#number3").val();
+    var guess4 = $("#number4").val();
+    $("#userChoice").text(guess1+ guess2+ guess3+guess4);
+    
+});
