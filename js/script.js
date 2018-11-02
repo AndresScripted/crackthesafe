e8c4a3a1d38a881aecbfe04471fa39da8fcf1aa6
Andres <andreshuz0709@gmail.com>
2018-11-02 01:25:57 +0000

    trouble with variables

--- js/script.js
+++ js/script.js
@@ -4,13 +4,17 @@
 
 //GLOBAL VARIABLES
 /* global $ */
-var randomNumber1 = Math.random();
+var randomNumber1 = Math.floor(Math.random() * 10) +1;
 console.log(randomNumber1);
-var randomNumber2 = Math.random();
-var randomNumber3 = Math.random();
-var randomNumber4 = Math.random();
+var randomNumber2 = Math.floor(Math.random() * 10) +1;
+console.log(randomNumber2);
+var randomNumber3 = Math.floor(Math.random() * 10) +1;
+console.log(randomNumber3);
+var randomNumber4 = Math.floor(Math.random() * 10) +1;
+console.log(randomNumber4);
 
 let numberGuess1 = " ";
+
 let numberGuess2 = " ";
 let numberGuess3 = " ";
 let numberGuess4 = " ";
@@ -23,9 +27,27 @@ let numbersInPlace = 0;
 $("button").click(function() {
     
     numberGuess1 = $("#number1").val();
+    let integer1 = parseInt(numberGuess1);
     numberGuess2 = $("#number2").val();
+    let integer2 = parseInt(numberGuess2);
     numberGuess3 = $("#number3").val();
+    let integer3 = parseInt(numberGuess3);
     numberGuess4 = $("#number4").val();
+    let integer4 = parseInt(numberGuess4);
     $("#userChoice").text(numberGuess1+ " " + numberGuess2 + " " + numberGuess3 + " " + numberGuess4);
+    
+    if(integer1 === randomNumber1){
+        numbersInPlace = numbersInPlace + 1
+        $("#results").text(numbersInPlace);
+    } else if(integer2 === randomNumber2){
+        numbersInPlace = numbersInPlace + 1
+        $("#results").text(numbersInPlace);
+    }else if (integer3 === randomNumber3){
+        numbersInPlace = numbersInPlace + 1
+        $("#results").text(numbersInPlace);
+    }else if(integer4 === randomNumber4){
+        numbersInPlace = numbersInPlace + 1
+        $("#result").text(numbersInPlace);
+    }
     
 });
